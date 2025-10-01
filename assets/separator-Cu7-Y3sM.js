849602d4d199471272f3538cc1ec5961a8f1b024
yusuke-kurosawa <kurosawa@sas-com.com>
2025-10-01 17:00:16 +0900

    Updates

---
+++ assets/separator-Cu7-Y3sM.js
@@ -0,0 +1,1 @@
+import{r as a,j as r,bY as o}from"./react-CBomtcya.js";import{d as e}from"./index-CDZW56YC.js";const s=a.forwardRef(({className:a,orientation:s="horizontal",decorative:i=!0,...t},l)=>r.jsx(o,{ref:l,decorative:i,orientation:s,className:e("shrink-0 bg-border","horizontal"===s?"h-[1px] w-full":"h-full w-[1px]",a),...t}));s.displayName=o.displayName;export{s as S};
